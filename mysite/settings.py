6ef832230db0ea20403bfcf6b2c5a2a56119c245
Ke <kd19850511@gmail.com>
2018-08-06 06:49:59 -0400

    fix menu

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print (STATIC_ROOT)
 LOGIN_REDIRECT_URL = '/'
 
