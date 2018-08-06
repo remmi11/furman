c40df6b1130f213f6bc52d637927c5e07e3cc1fa
Ke <kd19850511@gmail.com>
2018-08-06 05:45:11 -0400

    add host

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'new',
+        'NAME': 'furmandb',
         'USER': 'furman',
         'PASSWORD': 'gdipass1',
         'HOST': '13.82.41.95',
