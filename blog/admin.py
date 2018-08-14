220140a2754ff521295372bf934df3540aaa4205
Ke <kd19850511@gmail.com>
2018-08-14 14:36:53 -0400

    database changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 #from .models import Post
-<<<<<<< HEAD
 from .models import FormAll
 
 #admin.site.register(Post)
 admin.site.register(FormAll)
-=======
-from .models import Form
-
-#admin.site.register(Post)
-admin.site.register(Form)
->>>>>>> f49c2818e3e81e289622011a04b655ec5cd1e91f
